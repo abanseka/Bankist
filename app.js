6ffc65310722b38751e7f0d81c819219dd1cec72
abanseka <sylvester@archis.company>
2022-02-22 23:23:11 +0100

    Implement Login

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
+import accounts from "./scripts/accounts";
+import { createUsernames, login } from "./scripts/login";
 
+const init = function () {
+  createUsernames(accounts);
+  login(accounts);
+};
+
+init();
+
+// console.log(accounts);
---
+++ scripts/login.js
@@ -0,0 +1,35 @@
+import {
+  appContainer,
+  greetings,
+  loginBtn,
+  userName,
+  userPassword,
+} from "./elements";
+
+let currentAccount;
+export const createUsernames = function (acc) {
+  Object.values(acc).forEach(function (acc) {
+    return (acc.username = acc.owner
+      .toLowerCase()
+      .split(" ")
+      .map((nname) => nname[0])
+      .join(""));
+  });
+};
+
+export const login = function (acc) {
+  loginBtn.addEventListener("click", function (e) {
+    e.preventDefault();
+    currentAccount = Object.values(acc).find(function (acc) {
+      return acc.username === userName.value;
+    });
+
+    if (currentAccount?.pin === Number(userPassword.value)) {
+      greetings.textContent = `Welcome Back, ${
+        currentAccount.owner.split(" ")[0]
+      }`;
+      appContainer.style.opacity = 100;
+      userName.value = userPassword.value = "";
+    }
+  });
+};
